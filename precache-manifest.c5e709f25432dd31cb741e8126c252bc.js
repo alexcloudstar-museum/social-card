33f7119afed67a12145f508c9b8334e164bcc374
alexandrurdev <alexcloudstar@gmail.com>
2020-01-30 20:22:24 +0200

    Updates

---
+++ precache-manifest.c5e709f25432dd31cb741e8126c252bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10989d9ffbbf527a29a6e086f49d305",
+    "url": "/social-card/index.html"
+  },
+  {
+    "revision": "d60e78e4d14d6a57251e",
+    "url": "/social-card/static/css/main.f6956d10.chunk.css"
+  },
+  {
+    "revision": "7f12ff8f59a573e119c8",
+    "url": "/social-card/static/js/2.ee73dbad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/social-card/static/js/2.ee73dbad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d60e78e4d14d6a57251e",
+    "url": "/social-card/static/js/main.e0bcb26c.chunk.js"
+  },
+  {
+    "revision": "ba75a3728a1256ef6eb6",
+    "url": "/social-card/static/js/runtime-main.fd6f3049.js"
+  }
+]);
