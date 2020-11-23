c52eb2c50a8b6d40a2640eac5dd3c45dbfcb2ce9
Alex Cloudstar <58813782+alexcloudstar@users.noreply.github.com>
2020-11-23 22:10:27 +0200

    Wired Up Redux

---
+++ src/store/types.ts
@@ -0,0 +1,41 @@
+export const GET_USER = 'GET_USER';
+export const GET_POST = 'GET_POST';
+export const SET_LOADING = 'SET_LOADING';
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  website: string;
+}
+
+export interface Post {
+  title: string;
+  body: string;
+}
+
+export interface UserState {
+  data: User | null;
+  loading: boolean;
+}
+
+interface GetUserAction {
+  type: typeof GET_USER;
+  payload: User;
+}
+
+export interface PostState {
+  data: User | null;
+  loading: boolean;
+}
+
+interface GetPostAction {
+  type: typeof GET_POST;
+  payload: User;
+}
+
+interface SetLoadingAction {
+  type: typeof SET_LOADING;
+}
+
+export type Actions = GetUserAction | GetPostAction | SetLoadingAction;
