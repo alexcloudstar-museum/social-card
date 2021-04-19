4a2cd84c5c6f8b7165ca3cf91b2b9f414e289388
alexcloudstar <alexcloudstar@gmail.com>
2021-04-19 20:13:24 +0300

    Deleted && refreshed all folders, moved unnecessary prod deps to dev deps

--- src/components/post/ShortDescription.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { UserAndPostProps } from '../../types';
-
-export const ShortDescription: React.FC<UserAndPostProps> = ({ body }) => {
-  return <>{body && <p>{body}</p>}</>;
-};
